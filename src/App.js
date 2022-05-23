5b1cf98578f4eff85bb08585a37d3dfef26ce607
Jacqueline Guo <jacquelineguo@Jacquelines-MacBook-Pro.local>
2022-05-22 18:22:16 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import Row from 'react-bootstrap/Row';
+import {Container} from "react-bootstrap";
+import {Col} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Container className="align-middle mt-lg-5">
+          <Row>
+            <p class="fw-bold h1">
+              Fun with AI
+            </p>
+          </Row>
+          <Row>
+            <div className="form-group mt-3">
+              <label className="fw-bold" htmlFor="exampleFormControlTextarea1">
+                Enter prompt
+              </label>
+              <textarea className="form-control border border-dark mt-1"
+                        id="exampleFormControlTextarea1" rows="10"></textarea>
+              <button type="submit"
+                      className="btn btn-primary mt-sm-1 float-end btn-lg">
+                Submit
+              </button>
+            </div>
+          </Row>
+          <Row>
+            <p class="fw-bold h3">
+              Response
+            </p>
+          </Row>
+          <Row>
+            <div class="card bg-light">
+              <div class="row">
+                <div class="col-2 fw-bold">
+                  Prompt:
+                </div>
+                <div class="col-8">
+                  blablalba
+                </div>
+
+              </div>
+              <div className="row">
+                <div className="col-2 fw-bold">
+                  Response:
+                </div>
+                <div className="col-8">
+                  blablalba
+                </div>
+
+              </div>
+            </div>
+          </Row>
+        </Container>
+      </div>
   );
 }
 
